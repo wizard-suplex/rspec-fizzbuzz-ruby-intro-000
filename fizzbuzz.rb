352fb6a915951d5c4a426f9c6b63485116d6de0d
Alan Cooke <atc4@rice.edu>
2020-11-19 17:44:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,13 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    "Fizz"
+    puts "Fizz"
   elsif int % 5 == 0
-    "Buzz"
+    puts "Buzz"
   elsif int % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
+    puts "FizzBuzz"
   else
-    nil
+    puts nil
   end
 end
 
