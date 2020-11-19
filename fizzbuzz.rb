2b04628ef11ef7f41e34402852db79b7bf99a4f7
Alan Cooke <atc4@rice.edu>
2020-11-19 17:32:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizbuzz(number)
+def fizzbuzz(number)
   if number % 3 == 0
     puts "Fizz"
   elsif number % 5 == 0
