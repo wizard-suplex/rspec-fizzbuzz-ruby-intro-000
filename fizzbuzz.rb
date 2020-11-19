b5e5af04ac29d950002c006d023abb0fa33874d8
Alan Cooke <atc4@rice.edu>
2020-11-19 17:50:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 3 == 0
     return "Fizz"
   elsif int % 5 == 0
-    return "Buzz"
-  elsif int % 15 == 0
-    return "FizzBuzz"
+    return "Buzz" 
   else
     return nil
   end
